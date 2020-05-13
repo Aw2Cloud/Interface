888c8a956867e1f5afbe72104514bcc607704ce6
Aw2Cloud <bayu.st252@gmail.com>
2020-05-13 22:46:04 +0700

    Interface

---
+++ Interface.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface{
+    public class prints{
+        public string nama { get; set; }
+        public string ukuran { get; set; }
+    }
+    public interface IPrinterWindows{
+        void show(prints printer);
+        void print(prints printer);
+        
+    }
+    public class ConvertToEpson : IPrinterWindows{
+        public void show(prints printer){
+            printer.nama = "Epson";
+            printer.ukuran = "10*11";
+            Console.WriteLine("{0} display dimension : {1}", printer.nama,printer.ukuran);
+        }
+        public void print(prints printer){
+            printer.nama = "Epson";
+            printer.ukuran = "10*11";
+            Console.WriteLine("{0} printer printing...", printer.nama);
+        }
+}
+    public class ConvertToCanon : IPrinterWindows{
+        public void show(prints printer){
+            printer.nama = "Canon";
+            printer.ukuran = "9.5*12";
+            Console.WriteLine("{0} display dimension : {1}", printer.nama, printer.ukuran);
+        }
+        public void print(prints printer){
+            printer.nama = "Canon";
+            printer.ukuran = "9.5*12";
+            Console.WriteLine("{0} printer printing...", printer.nama);
+        }
+    }
+    public class ConvertToLaserjet : IPrinterWindows{
+        public void show(prints printer){
+            printer.nama = "LeserJet";
+            printer.ukuran = "12*12";
+            Console.WriteLine("{0} display dimension : {1}", printer.nama, printer.ukuran);
+        }
+        public void print(prints printer){
+            printer.nama = "LeserJet";
+            printer.ukuran = "12*12";
+            Console.WriteLine("{0} printer printing...", printer.nama);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface{
+    class Program{
+        static void Main(string[] args){
+            prints printer = new prints();
+            printer.nama = "Printer";
+            printer.ukuran = "0*0";
+
+
+            IPrinterWindows convert;
+
+            Console.WriteLine("Pilih Printer: ");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Canon");
+            Console.WriteLine("3. LaserJet\n");
+
+            Console.Write("Nomer Printer [1..3] : ");
+            int nomorPrinter = Convert.ToInt32(Console.ReadLine());
+
+            if (nomorPrinter == 1)
+                convert = new ConvertToEpson();
+            else if (nomorPrinter == 2)
+                convert = new ConvertToCanon();
+            else
+                convert = new ConvertToLaserjet();
+
+            convert.show(printer);
+            convert.print(printer);
+
+            Console.ReadKey();
+        }
+    }
+}
